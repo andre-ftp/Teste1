9430ed4642ed5d478546e8f61e1bfe33684e48fb
Grims <grims@vivaldi.net>
2024-01-02 11:37:15 +0000

    Nova Feat

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste Artigos - Laravel</title>
+    <title>Nova Feat - Laravel</title>
     @vite('resources/css/app.css')
 </head>
 

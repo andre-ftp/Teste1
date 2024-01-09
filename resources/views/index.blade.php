1c5d1bf3e861501b6c107c0258892b80deb18ced
Grims <grims@vivaldi.net>
2024-01-09 14:25:41 +0000

    Ajuste Título

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Posts - Laravel</title>
+    <title>Posts-Laravel</title>
     @vite('resources/css/app.css')
 </head>
 
